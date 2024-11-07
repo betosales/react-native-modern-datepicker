e26401ab305691576993839cb0b0c459cb8de0d7
betosales <oemaildobeto@gmail.com>
2024-11-07 10:19:52 -0300

    fix index.d.ts

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,62 +1,63 @@
-import type {FunctionComponent} from 'react';
+import type { FunctionComponent } from "react";
 import type { StyleProp, ViewStyle } from "react-native";
-
-declare const DatePicker:FunctionComponent
-export default DatePicker
-export declare function getFormatedDate(date?: Date, format?: string):string
-export declare function getToday():string
 
 export interface DatePickerProps {
   /** Gets called when selected value changes */
-  onSelectedChange: (dateString: string) => void
+  onSelectedChange?: (dateString: string) => void;
   /** Gets called when month changes */
-  onMonthYearChange: (dateString: string) => void
+  onMonthYearChange?: (dateString: string) => void;
   /** Gets called when time changes */
-  onTimeChange: (dateString: string) => void
+  onTimeChange?: (dateString: string) => void;
   /** Gets called when date changes */
-  onDateChange: (dateString: string) => void
+  onDateChange?: (dateString: string) => void;
 
   /** Initially visible month */
-  current: string
+  current?: string;
   /** The primary value of the date picker */
-  selected: string
+  selected?: string;
   /** Specifies the minimum selectable day by user */
-  minimumDate: string
+  minimumDate?: string;
   /** Specifies the maximum selectable day by user */
-  maximumDate: string
-  
+  maximumDate?: string;
+
   /** The minimum selectable year when user opens the year selector */
-  selectorStartingYear: number
-  /** The maxium selectable year when user opens the year selector */
-  selectorEndingYear: number
-  
+  selectorStartingYear?: number;
+  /** The maximum selectable year when user opens the year selector */
+  selectorEndingYear?: number;
+
   /** Disable month & year from being changed */
-  disableDateChange: boolean
+  disableDateChange?: boolean;
   /** Switches between 'Gregorian' & 'Jalaali' mode */
-  isGregorian: boolean
-  
+  isGregorian?: boolean;
+
   /** Customizing date picker style */
-  options: Partial<DatePickerOptions>
-  
-  mode: 'datepicker' | 'calendar' | 'monthYear' | 'time'
-  
-  minuteInterval: 1 | 2 | 3 | 4 | 5 | 6 | 10 | 12 | 15 | 20 | 30 | 60 
-  
+  options?: Partial<DatePickerOptions>;
+
+  mode?: "datepicker" | "calendar" | "monthYear" | "time";
+
+  minuteInterval?: 1 | 2 | 3 | 4 | 5 | 6 | 10 | 12 | 15 | 20 | 30 | 60;
+
   /** Changes date picker container style */
-  style: StyleProp<ViewStyle>
+  style?: StyleProp<ViewStyle>;
 }
 
 export interface DatePickerOptions {
-  textHeaderColor: string
-  textDefaultColor: string
-  selectedTextColor: string
-  mainColor: string
-  textSecondaryColor: string
-  borderColor: string
-  defaultFont: string
-  headerFont: string
-  textFontSize: number
-  textHeaderFontSize: number
-  headerAnimationDistance: number
-  daysAnimationDistance: number
+  textHeaderColor?: string;
+  textDefaultColor?: string;
+  selectedTextColor?: string;
+  mainColor?: string;
+  textSecondaryColor?: string;
+  borderColor?: string;
+  defaultFont?: string;
+  headerFont?: string;
+  textFontSize?: number;
+  textHeaderFontSize?: number;
+  headerAnimationDistance?: number;
+  daysAnimationDistance?: number;
 }
+
+declare const DatePicker: FunctionComponent<DatePickerProps>;
+export default DatePicker;
+
+export function getFormatedDate(date?: Date, format?: string): string;
+export function getToday(): string;
